32acf7f888cf7db808dcf11f372f95b2f2cedfd8
navee43 <naveenyadav11k@gmail.com>
2025-03-31 17:42:32 +0530

    Update constant.js

--- frontend/src/redux/constant.js
+++ frontend/src/redux/constant.js
@@ -1,4 +1,4 @@
-export const BASE_URL = ''
+export const BASE_URL = 'https://chat-app-backend-8vht.onrender.com'
 export const MAIN_URL = '/api/user'
 export const CHAT_URL = '/api/chat'
 export const MESSAGE_URL = '/api/message'
