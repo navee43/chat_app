6642bcbe7fbae64e715279e253c64c0ef5600604
navee43 <naveenyadav11k@gmail.com>
2025-06-23 11:10:35 +0530

    Update apiSlice.js

--- frontend/src/redux/api/apiSlice.js
+++ frontend/src/redux/api/apiSlice.js
@@ -2,15 +2,19 @@ import { createApi } from "@reduxjs/toolkit/query/react";
 import { fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { BASE_URL } from "../constant.js";
 
-const apiSlice = createApi({
-    baseQuery:fetchBaseQuery({baseUrl:BASE_URL , credentials: "include"}),
-    prepareHeaders: (headers, { getState }) => {
-    const token = getState().auth.token;  // 🔹 Retrieve token from Redux state
-    if (token) {
-      headers.set("Authorization", `Bearer ${token}`);  // 🔹 Attach token
-    }
-    return headers;
-  },
+const apiSlice = createApi({    
+    baseQuery:fetchBaseQuery({baseUrl:BASE_URL ,    
+            prepareHeaders: (headers, { getState }) => {
+      const token = getState()?.auth?.userInfo?.data?.accessToken; 
+      console.log("the token is " , getState()?.auth?.userInfo?.data?.accessToken )
+
+      if (token) {
+        headers.set("Authorization", `Bearer ${token}`);
+      }
+
+      return headers;
+    },
+    }),
 endpoints:()=>({})
 })
 
