4772314789284fce7b9f6e6f399d35f3c2808c3e
navee43 <naveenyadav11k@gmail.com>
2025-06-24 09:28:19 +0530

    Update constant.js

--- frontend/src/redux/constant.js
+++ frontend/src/redux/constant.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'https://chad-chat.onrender.com'
+export const BASE_URL = 'https://chat-app-kappa-snowy.vercel.app/'
 export const MAIN_URL = '/api/user'
 export const CHAT_URL = '/api/chat'
 export const MESSAGE_URL = '/api/message'
