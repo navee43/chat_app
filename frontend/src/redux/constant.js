54ce8ebbeaba74568dad8b2d1388ba49a9b17722
navee43 <naveenyadav11k@gmail.com>
2025-06-24 16:39:13 +0530

    Update constant.js

--- frontend/src/redux/constant.js
+++ frontend/src/redux/constant.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'chat-app-orcin-seven.vercel.app'
+export const BASE_URL = 'https://chat-app-ebjn.onrender.com'
 export const MAIN_URL = '/api/user'
 export const CHAT_URL = '/api/chat'
 export const MESSAGE_URL = '/api/message'
